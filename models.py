17fb01badfdb67690662a1cf99d7e1cd673d3726
dev-swd <tladnjsen@gmail.com>
2021-09-08 04:11:49 +0900

    Add blank model class

---
+++ models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class MainMenu(models.Model):
+    pass
+
+
+class FirstCategory(models.Model):
+    pass
+
+
+class SecondCategory(models.Model):
+    pass
+
+
+class ThirdCategory(models.Model):
+    pass
