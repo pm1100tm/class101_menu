8beb507fb4c68f0548bf7579dd60b6da702d6562
dev-swd <tladnjsen@gmail.com>
2021-09-09 02:55:16 +0900

    [menu:swd] declare fields of menu model - MainMenu, FirstCategory, SecondCategory, ThirdCategory

--- models.py
+++ models.py
@@ -2,16 +2,49 @@ from django.db import models
 
 
 class MainMenu(models.Model):
-    pass
+    id         = models.PositiveSmallIntegerField(primary_key=True)
+    name       = models.CharField(max_length=12, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table     = 'main_menus'
+        verbose_name = 'main_menus_table'
 
 
 class FirstCategory(models.Model):
-    pass
+    id           = models.PositiveSmallIntegerField(primary_key=True)
+    name         = models.CharField(max_length=12, unique=True)
+    index_number = models.PositiveSmallIntegerField()
+    is_deleted   = models.BooleanField(default=False)
+    created_at   = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table     = 'first_category'
+        verbose_name = 'first_category_table'
+        ordering     = ['index_number']
 
 
 class SecondCategory(models.Model):
-    pass
+    id           = models.PositiveSmallIntegerField(primary_key=True)
+    name         = models.CharField(max_length=20, unique=True)
+    index_number = models.PositiveSmallIntegerField()
+    is_deleted   = models.BooleanField(default=False)
+    created_at   = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table     = 'second_category'
+        verbose_name = 'second_category_table'
+        ordering     = ['index_number']
 
 
 class ThirdCategory(models.Model):
-    pass
+    id           = models.PositiveSmallIntegerField(primary_key=True)
+    name         = models.CharField(max_length=20, unique=True)
+    index_number = models.PositiveSmallIntegerField()
+    is_deleted   = models.BooleanField(default=False)
+    created_at   = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'third_category'
+        verbose_name = 'third_category_table'
+        ordering = ['index_number']
