766dd6de892c13b16cc53bd8491ac8668aee5fd6
dev-swd <tladnjsen@gmail.com>
2021-09-17 23:55:51 +0900

    [menu:swd] add base data for menu

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ class MainMenu(models.Model):
     class Meta:
         db_table     = 'main_menus'
         verbose_name = 'main_menus_table'
+        ordering     = ['id']
 
 
 class FirstCategory(models.Model):
@@ -17,6 +18,7 @@ class FirstCategory(models.Model):
     index_number = models.PositiveSmallIntegerField()
     is_deleted   = models.BooleanField(default=False)
     created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table     = 'first_category'
@@ -25,11 +27,13 @@ class FirstCategory(models.Model):
 
 
 class SecondCategory(models.Model):
-    id           = models.PositiveSmallIntegerField(primary_key=True)
-    name         = models.CharField(max_length=20, unique=True)
-    index_number = models.PositiveSmallIntegerField()
-    is_deleted   = models.BooleanField(default=False)
-    created_at   = models.DateTimeField(auto_now_add=True)
+    id             = models.PositiveSmallIntegerField(primary_key=True)
+    name           = models.CharField(max_length=20, unique=True)
+    index_number   = models.PositiveSmallIntegerField()
+    is_deleted     = models.BooleanField(default=False)
+    created_at     = models.DateTimeField(auto_now_add=True)
+    updated_at     = models.DateTimeField(auto_now=True)
+    first_category = models.ForeignKey('FirstCategory', related_name="first_category", on_delete=models.CASCADE)
 
     class Meta:
         db_table     = 'second_category'
@@ -38,11 +42,13 @@ class SecondCategory(models.Model):
 
 
 class ThirdCategory(models.Model):
-    id           = models.PositiveSmallIntegerField(primary_key=True)
-    name         = models.CharField(max_length=20, unique=True)
-    index_number = models.PositiveSmallIntegerField()
-    is_deleted   = models.BooleanField(default=False)
-    created_at   = models.DateTimeField(auto_now_add=True)
+    id              = models.PositiveSmallIntegerField(primary_key=True)
+    name            = models.CharField(max_length=20, unique=True)
+    index_number    = models.PositiveSmallIntegerField()
+    is_deleted      = models.BooleanField(default=False)
+    created_at      = models.DateTimeField(auto_now_add=True)
+    updated_at      = models.DateTimeField(auto_now=True)
+    second_category = models.ForeignKey('SecondCategory', related_name="second_category", on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'third_category'
